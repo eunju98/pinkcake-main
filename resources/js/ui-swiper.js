44c3cc387feb2ac4b298eff44648b3fbfede116b
eunju98 <dmswn1321@gmail.com>
2024-10-26 20:19:45 +0900

    first commit

---
+++ resources/js/ui-swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".slide-section04", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".slide-studio .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
